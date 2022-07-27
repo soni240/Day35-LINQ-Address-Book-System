0a7e7b4abef5c8b1490e573164035b5408e15a53
soni240 <s.fegade21@gmail.com>
2022-07-27 19:40:50 +0530

    [Gunvanti]Add:Add:create database

---
+++ Linq_Address_Book/DataTableManager.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Address_Book
+{
+    public class DataTableManager
+    {
+        DataTable custTable;
+        //Create Column in Datatable
+        public void CreateDataTable()
+        {
+            custTable = new DataTable("AddressBookSystem");
+            DataColumn dtColumn;
+
+            // Create id column  
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int32);
+            dtColumn.ColumnName = "Contactid";
+            dtColumn.Caption = "Contact ID";
+            dtColumn.AutoIncrement = true;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = true;
+            // Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "FirstName";
+            dtColumn.Caption = "First Name";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "LastName";
+            dtColumn.Caption = "Last Name";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "Address";
+            dtColumn.Caption = "Address";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "City";
+            dtColumn.Caption = "City";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Name column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "State";
+            dtColumn.Caption = "State";
+            dtColumn.AutoIncrement = false;
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            custTable.Columns.Add(dtColumn);
+
+            // Create Address column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(String);
+            dtColumn.ColumnName = "Email";
+            dtColumn.Caption = "Email";
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            custTable.Columns.Add(dtColumn);
+
+            // Create Address column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int64);
+            dtColumn.ColumnName = "PhoneNumber";
+            dtColumn.Caption = "PhoneNumber";
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            custTable.Columns.Add(dtColumn);
+
+            // Create Address column.    
+            dtColumn = new DataColumn();
+            dtColumn.DataType = typeof(Int64);
+            dtColumn.ColumnName = "Zip";
+            dtColumn.Caption = "Zip";
+            dtColumn.ReadOnly = false;
+            dtColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            custTable.Columns.Add(dtColumn);
+
+        }
+        //Insert Values in Datatable
+        public int AddValues()
+        {
+            CreateDataTable();
+            //Create Object for DataTable
+            ContactDataManager contactDataManager = new ContactDataManager();
+            ContactDataManager contactDataManagers = new ContactDataManager();
+            //Insert Values into Table
+            contactDataManager.FirstName = "Ankita";
+            contactDataManager.LastName = "V";
+            contactDataManager.PhoneNumber = 9842905050;
+            contactDataManager.Email = "ankita@gmail.com";
+            contactDataManager.Address = "4,B Block,Avadi";
+            contactDataManager.City = "chennai";
+            contactDataManager.State = "TN";
+            contactDataManager.zip = 600072;
+            InsertintoDataTable(contactDataManager);
+
+            //Insert Values into Table
+            contactDataManagers.FirstName = "john";
+            contactDataManagers.LastName = "k";
+            contactDataManagers.PhoneNumber = 7742905050;
+            contactDataManagers.Email = "john@gmail.com";
+            contactDataManagers.Address = "Sasthri street,ambattur";
+            contactDataManagers.City = "chennai";
+            contactDataManagers.State = "TN";
+            contactDataManagers.zip = 123001;
+            InsertintoDataTable(contactDataManagers);
+
+            return custTable.Rows.Count;
+        }
+
+        //Insert values in Data Table
+        public void InsertintoDataTable(ContactDataManager contactDataManager)
+        {
+            DataRow dtRow = custTable.NewRow();
+            dtRow["FirstName"] = contactDataManager.FirstName;
+            dtRow["LastName"] = contactDataManager.LastName;
+            dtRow["Address"] = contactDataManager.Address;
+            dtRow["City"] = contactDataManager.City;
+            dtRow["State"] = contactDataManager.State;
+            dtRow["Zip"] = contactDataManager.zip;
+            dtRow["PhoneNumber"] = contactDataManager.PhoneNumber;
+            dtRow["Email"] = contactDataManager.Email;
+            custTable.Rows.Add(dtRow);
+
+        }
+        //Display all Values in DataRow
+        public void Display()
+        {
+            foreach (DataRow dtRows in custTable.Rows)
+            {
+                Console.WriteLine("{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \t {7}\n", dtRows["FirstName"], dtRows["LastName"], dtRows["Address"], dtRows["City"], dtRows["State"], dtRows["Zip"], dtRows["PhoneNumber"], dtRows["Email"]);
+            }
+        }
+    }
+}
+
+    
+
---
+++ Linq_Address_Book/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Linq_AddressBook;
+
+Console.WriteLine("Welcome to LINQ AddressBookSystem!");
+//Create Object for DataTable
+ContactDataManager contactDataManager = new ContactDataManager();
+ContactDataManager contactDataManagers = new ContactDataManager();
+DataTableManager dataTableManger = new DataTableManager();
+dataTableManger.CreateDataTable();
+
+//Insert Values into Table
+contactDataManager.FirstName = "Ankita";
+contactDataManager.LastName = "V";
+contactDataManager.PhoneNumber = 9842905050;
+contactDataManager.Email = "Ankita@gmail.com";
+contactDataManager.Address = "4,B Block,Avadi";
+contactDataManager.City = "chennai";
+contactDataManager.State = "TN";
+contactDataManager.zip = 600072;
+dataTableManger.InsertintoDataTable(contactDataManager);
+
+//Insert Values into Table
+contactDataManagers.FirstName = "john";
+contactDataManagers.LastName = "k";
+contactDataManagers.PhoneNumber = 7742905050;
+contactDataManagers.Email = "john@gmail.com";
+contactDataManagers.Address = "sasthri street,ambattur";
+contactDataManagers.City = "chennai";
+contactDataManagers.State = "TN";
+contactDataManagers.zip = 123001;
+dataTableManger.InsertintoDataTable(contactDataManagers);
+dataTableManger.Display();
+        
+    
+
+
